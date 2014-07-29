7d22f4f08695b09b4824b2a4630a312cff190d1b
Matthew Bradbury <matthew.bradbury@me.com>
2014-07-29 08:46:15 +0100

    password reset tests complete

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -20,16 +20,13 @@ class PasswordResetsController < ApplicationController
 	def update
 	  @user = User.find_by_password_reset_token!(params[:id])
 	  
-
 	  if @user.password_reset_sent_at < 2.hours.ago
 	    redirect_to new_password_reset_path, :alert => "Password &crarr; 
 	      reset has expired."
 
-
 		elsif @user.update_attributes(user_params)
 
 			logger.debug "@userhash: #{@user.attributes.inspect}"
-
 
 	    redirect_to root_url, :notice => "Password has been reset."
 	  
--- spec/requests/password_resets_pages_spec.rb
+++ spec/requests/password_resets_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'Password reset' do
-	describe "shows password reset page" do
+	describe "shows forgotten password page" do
 
 	  before do
 			visit signin_path
@@ -14,7 +14,7 @@ describe 'Password reset' do
 
 	describe "sends password reset" do
 
-	  let(:user) { FactoryGirl.create(:user, :password_reset_token => "anything")  }
+	  let(:user) { FactoryGirl.create(:user)  }
 		before do
 			visit signin_path
 			click_link('forgotten password?')
@@ -25,7 +25,67 @@ describe 'Password reset' do
 			it {page.should have_title "Ruby on Rails Template"}
 	    it {page.should have_content("Email sent")}
 	    it {last_email.to.should include(user.email)}
-
 	end
 
+	describe "does not send password reset to invalid user" do
+
+	  let(:user) { FactoryGirl.create(:user)  }
+		before do
+			visit signin_path
+			click_link('forgotten password?')
+			fill_in 'Email', :with => "madeupuser@example.com"
+			click_button "Reset Password"
+		end
+
+			it {page.should have_title "Ruby on Rails Template"}
+	    it {page.should have_content("reset failed")}
+	    it {last_email.should be_nil}
+	end
+
+	describe "shows password reset page" do
+	  let(:user) { FactoryGirl.create(:user)}
+
+	  before do
+	  	user.send_password_reset
+	  	visit edit_password_reset_path(user.password_reset_token)
+	  end
+
+	  it {page.should have_content("Password confirmation")}
+	end
+
+	describe "resets password" do
+	  let(:user) { FactoryGirl.create(:user)}
+	
+	  before do
+	  	user.send_password_reset
+	  	visit edit_password_reset_path(user.password_reset_token)
+	  	fill_in 'Password', :with => "newpassword"
+	  	fill_in 'Password confirmation', :with => "newpassword"
+	  	click_button 'Update Password'
+	  end
+
+	  it "should sign in with new password" do 
+	  	visit signin_path
+	  	fill_in 'Email', :with => user.email
+	  	fill_in 'Password', :with => "newpassword"
+	  	click_button 'Sign in'
+	  	visit root_path
+
+	  	page.should have_content(user.name)
+	  end
+	
+	  it "should not sign in with old password" do 
+	  	visit signin_path
+	  	fill_in 'Email', :with => user.email
+	  	fill_in 'Password', :with => "foobar"
+	  	click_button 'Sign in'
+	  	visit root_path
+	  	
+	  	page.should have_content("Rails Template")
+	  end
+	end
+
+
 end
+
+
